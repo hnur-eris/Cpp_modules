3b7924c20c382f206ba399c8b17a3863641501d6
Hatice Nur Eris <110325247+hnur-eris@users.noreply.github.com>
2023-06-20 19:17:11 +0300

    .

---
+++ Cpp03/ex01/ScavTrap.hpp
@@ -0,0 +1,17 @@
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap:public ClapTrap
+{
+    public :
+        ScavTrap();
+        ScavTrap(const string &name);
+        ~ScavTrap();
+        ScavTrap &operator = (const ScavTrap &obj);
+        void guardGate();
+        void attack(const string &target);
+};
+
+#endif
