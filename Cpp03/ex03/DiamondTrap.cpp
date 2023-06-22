26dccee103542dbe111cc8afb5c065e58f9f82e0
Hatice Nur Eris <110325247+hnur-eris@users.noreply.github.com>
2023-06-23 00:58:21 +0300

    .

--- Cpp03/ex03/DiamondTrap.cpp
+++ Cpp03/ex03/DiamondTrap.cpp
@@ -2,12 +2,21 @@
 
 DiamondTrap::DiamondTrap()
 {
+    this->name = "DiamondTrop";
+    ClapTrap::name = this->name + "_clap_name";
+    hitPoint = FragTrap::hitPoint;
+    energyPoint = ScavTrap::energyPoint;
+    damage = FragTrap::damage;
+
     cout << "DiamondTrap default constructor called" << endl;
 }
 
-DiamondTrap::DiamondTrap(const string &name)
+DiamondTrap::DiamondTrap(const string &name) : ClapTrap(name + "_clap_name")
 {
     this->name = name;
+    this->hitPoint = FragTrap::hitPoint;
+    this->energyPoint = FragTrap::energyPoint;
+    this->damage = FragTrap::damage;
     cout << "DiamondTrap name constructor called" << endl; 
 }
 
@@ -20,9 +29,25 @@ DiamondTrap::DiamondTrap(const DiamondTrap &obj)
 
 DiamondTrap &DiamondTrap::operator = (const DiamondTrap &obj)
 {
-    if (*this != obj)
-    {
-        this->name = obj.name;
+    if (this == &obj)
+        return *this;
+    ClapTrap::name = obj.name + "_clap_name";
+    ScavTrap::name = obj.name;
+    FragTrap::name = obj.name;
+    name = obj.name;
+    this->energyPoint = obj.energyPoint;
+    this->hitPoint = obj.hitPoint;
+    this->damage = obj.damage;
+    return *this;
 
-    }
+}
+
+void DiamondTrap::whoAmI()
+{
+    cout << "DiamondTrap " << name << " is actually ClapTrap " << ClapTrap::name << endl;
+}
+
+DiamondTrap::~DiamondTrap()
+{
+    cout << "DiamondTrap destructor called " << endl;
 }
