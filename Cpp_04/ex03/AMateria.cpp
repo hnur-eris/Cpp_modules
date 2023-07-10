c468664e2a7cf828eaeb2551e70b94d535e9b721
Hatice Nur Eris <110325247+hnur-eris@users.noreply.github.com>
2023-07-10 13:49:32 +0300

    .

---
+++ Cpp_04/ex03/AMateria.cpp
@@ -0,0 +1,32 @@
+#include "AMateria.hpp"
+
+AMateria::AMateria(string const &type)
+{
+    this->type = type;
+}
+
+AMateria::AMateria(const AMateria &obj)
+{
+    type = obj.type;
+}
+
+AMateria::~AMateria()
+{
+
+}
+
+AMateria &AMateria::operator = (const AMateria &obj)
+{
+    this->type = obj.type;
+    return *this;
+}
+
+string const &AMateria::getType() const
+{
+    return this->type;
+}
+
+void AMateria::use(ICharacter &target)
+{
+    (void)target;
+}
