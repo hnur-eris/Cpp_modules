901a0d7d37b0434207098cfd44aa0c12dbcb09c0
Hatice Nur Eris <110325247+hnur-eris@users.noreply.github.com>
2024-02-06 22:38:12 +0300

    .

--- Cpp_05/ex00/main.cpp
+++ Cpp_05/ex00/main.cpp
@@ -1,13 +1,21 @@
 #include "Bureaucrat.hpp"
 
-int main()
-{
-    try {
-        Bureaucrat nur("nur", 159);
-        cout << nur << endl;
+int main() {
+    
+    try
+    {
+        Bureaucrat potter("harry", 3);
+        cout << potter << endl;
+
+        potter.incrementDegree();
+        potter.decrementDegree();
+
+        Bureaucrat person("mistery" , 250);
+        cout << person << endl;
     }
-    catch (const std::exception &e) {
+    catch(const std::exception &e)
+    {
         std::cerr << e.what() << endl;
     }
-    return 0;
+    
 }
