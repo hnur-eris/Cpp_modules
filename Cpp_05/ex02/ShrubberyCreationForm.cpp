9465697f3e5a64da100991221b35c776b97c32a7
Hatice Nur Eris <110325247+hnur-eris@users.noreply.github.com>
2024-02-07 21:54:03 +0300

    .

---
+++ Cpp_05/ex02/ShrubberyCreationForm.cpp
@@ -0,0 +1,57 @@
+#include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm() 
+{
+    cout << "Shrubbery default constructor" << endl;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const string &tar) :  AForm("Shrubbery Creation Form", 145, 137), target(tar)
+{
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &obj) : AForm("Shrubbery Creation Form", 145, 137) {
+    *this = obj;
+} 
+
+ShrubberyCreationForm &ShrubberyCreationForm::operator= (const ShrubberyCreationForm &obj) {
+    this->target = obj.target;
+    return *this;
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm() {
+}
+
+void ShrubberyCreationForm::execute(Bureaucrat const &executor) const {
+    if (!getIsSigned()){
+        throw AForm::GradeTooLowException();
+    }
+    
+    if (executor.getGrade() > getGradeToExecute())
+        throw AForm::GradeTooLowException();
+    
+    
+    string str = target + "_shrubbery";
+    std::ofstream outputFile(str.c_str());
+
+
+    if (outputFile.is_open())
+    {
+        outputFile << "          &&& &&  & &&\n";
+        outputFile << "      && &//&||& ()|/ @, &&\n";
+        outputFile << "      &//(/&/&||/& /_/)_&/_&\n";
+        outputFile << "   &() &//&|()|/&/// '%' & ()\n";
+        outputFile << "  &_/_&&_/ |& |&&/&__%_/_& &&\n";
+        outputFile << "&&   && & &| &| /& & % ()& /&&\n";
+        outputFile << " ()&_---()&/&/|&&-&&--%---()~\n";
+        outputFile << "     &&     /|||\n";
+        outputFile << "             |||\n";
+        outputFile << "             |||\n";
+        outputFile << "             |||\n";
+        outputFile << "       , -=-~  .-^- _\n";
+        outputFile.close();
+		cout << "Shrubbery created for " << target << "." << endl;
+    }
+    else
+        cout << "Errorrr" << endl;
+    
+}
