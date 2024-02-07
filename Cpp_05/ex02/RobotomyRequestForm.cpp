c15e97f1b8c1b5c40cd312f22e7a217b3dff34d5
Hatice Nur Eris <110325247+hnur-eris@users.noreply.github.com>
2024-02-07 21:53:40 +0300

    .

---
+++ Cpp_05/ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,38 @@
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm()
+{
+    cout << "default constructor called" << endl;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const string &tar) : AForm("Robotmy Request Form" , 72, 45), target(tar) {
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &obj): AForm("Robotmy Request Form" , 72, 45){
+    *this = obj;
+}
+
+RobotomyRequestForm &RobotomyRequestForm::operator= (const RobotomyRequestForm &obj) 
+{
+    *this = obj;
+    return *this;
+}
+
+RobotomyRequestForm::~RobotomyRequestForm(){
+}
+
+void RobotomyRequestForm::execute(Bureaucrat const & executor) const {
+
+    if (!getIsSigned())
+		throw AForm::GradeTooLowException();
+
+	if (executor.getGrade() > getGradeToExecute())
+		throw AForm::GradeTooLowException();
+    
+        cout << "Some drilling noises" << endl;
+        if (rand() % 2 == 0)
+    		cout << target << " has been robotomized successfully." << endl;
+        else
+        	cout << "Robotomy on " << target << " has failed." << endl;
+
+}
---
+++ Cpp_05/ex02/RobotomyRequestForm.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "AForm.hpp"
+#include <cstdlib>
+
+class RobotomyRequestForm : public AForm
+{
+    private :
+        string target;
+    
+    public :
+        RobotomyRequestForm();
+        RobotomyRequestForm(const string &tar);
+        RobotomyRequestForm(const RobotomyRequestForm &obj);
+        RobotomyRequestForm &operator= (const RobotomyRequestForm &obj);
+        ~RobotomyRequestForm();
+
+        void execute(Bureaucrat const & executor) const;
+};
