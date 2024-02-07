7600b6f38eb22e4c226c40d3a556b39e2a3aae1a
Hatice Nur Eris <110325247+hnur-eris@users.noreply.github.com>
2024-02-08 00:15:06 +0300

    .

---
+++ Cpp_05/ex03/Intern.cpp
@@ -0,0 +1,53 @@
+#include "Intern.hpp"
+
+Intern::Intern(){
+}
+
+Intern::Intern(const Intern &obj) {
+    *this = obj;
+}
+Intern &Intern::operator= (const Intern &obj) {
+    *this = obj;
+    return *this;
+}
+
+Intern::~Intern() {
+}
+
+const char *Intern::InvalidArgument::what() const throw() {
+    return "Invalid argumenttt !!!\n";
+}
+
+AForm *Intern::makeForm(const string &formName, const  string &formTarget) {
+    string names [3] = {
+        "shrubbery creation",
+		"robotomy request",
+		"presidential pardon"
+        };
+    int i = 0;
+    while ( i < 3)
+    {
+        if (names[i] == formName)
+            break;
+        i++;
+    }
+    AForm *tmp = NULL;
+    switch (i)
+    {
+        case 0:
+            tmp = new ShrubberyCreationForm(formTarget);
+            break;
+        case 1:
+            tmp = new RobotomyRequestForm(formTarget);
+            break;
+        case 2:
+            tmp = new PresidentialPardonForm(formTarget);
+            break;
+        default:
+            throw Intern::InvalidArgument();
+            break;
+
+    }
+    cout << "Intern creates " << tmp->getName() << endl;
+    return tmp;
+}
---
+++ Cpp_05/ex03/Intern.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+
+class Intern {
+    
+    public :
+        Intern();
+        Intern(const Intern &obj);
+        Intern &operator = (const Intern &obj);
+        ~Intern();
+
+        AForm *makeForm(const string &formName, const string &formTarget);
+        class InvalidArgument : public std::exception {
+            public :
+                virtual const char *what() const throw();
+        };
+
+};
