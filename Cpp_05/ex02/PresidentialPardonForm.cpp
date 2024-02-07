fd2bc54adca8154e9bf69425c4e4b85adc32be99
Hatice Nur Eris <110325247+hnur-eris@users.noreply.github.com>
2024-02-07 21:53:18 +0300

    .

---
+++ Cpp_05/ex02/PresidentialPardonForm.cpp
@@ -0,0 +1,33 @@
+#include "PresidentialPardonForm.hpp"
+
+PresidentialPardonForm::PresidentialPardonForm()
+{
+    cout << "default constructor called" << endl;
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const string &tar) :  AForm("Presidental Pardon Form" , 25, 5),target(tar) {
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &obj) : AForm("Presidental Pardon Form" , 25, 5) {
+    *this = obj;
+}
+
+PresidentialPardonForm &PresidentialPardonForm::operator= (const PresidentialPardonForm &obj) {
+    *this = obj;
+    return *this;
+}
+
+void PresidentialPardonForm::execute(Bureaucrat const &executor) const {
+    
+    if (!getIsSigned())
+		throw AForm::GradeTooLowException();
+
+	if (executor.getGrade() > getGradeToExecute())
+		throw AForm::GradeTooLowException();
+    cout << target << " has been pardoned by Zafod Beeblebrox." << endl;
+}
+
+
+PresidentialPardonForm::~PresidentialPardonForm(){
+
+}
---
+++ Cpp_05/ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm {
+    private:
+        string target;
+    
+    public :
+        PresidentialPardonForm();
+        PresidentialPardonForm(const string &tar);
+        PresidentialPardonForm(const PresidentialPardonForm &obj);
+        PresidentialPardonForm &operator= (const PresidentialPardonForm &obj);
+        ~PresidentialPardonForm();
+
+        void execute(Bureaucrat const &executor ) const;
+};
