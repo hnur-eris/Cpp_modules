c1e1d67174e976e8daa6f1a195b0de2208f81699
Hatice Nur Eris <110325247+hnur-eris@users.noreply.github.com>
2024-02-07 21:52:23 +0300

    .

---
+++ Cpp_05/ex02/AForm.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Bureaucrat.hpp"
+#include <fstream>
+
+class Bureaucrat;
+class AForm {
+
+    private :
+        const string name;
+        bool isSigned;
+        const int gradeToSign;
+        const int gradeToExecute;
+
+    public :
+        AForm();
+        AForm(const string fName, const int grToSign, const int grToExec);
+        AForm(const AForm &obj);
+        AForm &operator = (const AForm &obj);
+        ~AForm();
+
+        string getName() const;
+        bool getIsSigned() const;
+        int getGradeToSign() const;
+        int getGradeToExecute() const;
+
+        void beSigned(Bureaucrat &bureaucrat);
+        virtual void execute(Bureaucrat const &executor) const = 0;
+
+        class GradeTooHighException : public std::exception {
+            virtual const char *what() const throw();
+        };
+
+        class GradeTooLowException : public std::exception {
+            virtual const char *what() const throw();
+        };
+
+};
+
+std::ostream& operator<<(std::ostream &out, AForm &Aform);
