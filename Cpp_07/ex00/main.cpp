b06084a02899f6b36e7c32746225f350b5e3fe28
Hatice Nur Eris <110325247+hnur-eris@users.noreply.github.com>
2024-02-16 19:21:23 +0300

    .

--- Cpp_07/ex00/main.cpp
+++ Cpp_07/ex00/main.cpp
@@ -9,31 +9,38 @@ void swap(T &a, T &b){
 
 template <typename M>
 M min(M a, M b) {
+    if (a == b)
+        return b;
+    
     return a < b ? a : b;
 }
 
 template <typename N>
 N max(N a, N b){
+    if (a == b)
+        return b;
+    
     return a > b ? a : b;
 }
 
-int main( void ) {
+int main() {
     int a = 2;
     int b = 3;
 
     ::swap( a, b );
     
-    cout << "a = " << a << ", b = " << b <<     endl;
-    cout << "min( a, b ) = " << ::min( a, b ) <<    endl;
-    cout << "max( a, b ) = " << ::max( a, b ) <<    endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    cout << "min( a, b ) = " << ::min( a, b ) << endl;
+    cout << "max( a, b ) = " << ::max( a, b ) << endl;
     
     string c = "chaine1";
     string d = "chaine2";
     
     ::swap(c, d);
     
-    cout << "c = " << c << ", d = " << d <<     endl;
-    cout << "min( c, d ) = " << ::min( c, d ) <<    endl;
-    cout << "max( c, d ) = " << ::max( c, d ) <<    endl;
+    cout << "c = " << c << ", d = " << d << endl;
+    cout << "min( c, d ) = " << ::min( c, d ) << endl;
+    cout << "max( c, d ) = " << ::max( c, d ) << endl;
+
     return 0;
 }
