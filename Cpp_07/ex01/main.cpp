65c4cc786b7e07da83bee67c3f5318b2b6d5c22e
Hatice Nur Eris <110325247+hnur-eris@users.noreply.github.com>
2024-02-16 19:59:33 +0300

    .

--- Cpp_07/ex01/main.cpp
+++ Cpp_07/ex01/main.cpp
@@ -1,8 +1,8 @@
 #include "iter.hpp"
 
 
-template <typename A>
-void iter(A *arr, unsigned long lenght, void (*fun)(A))
+template <typename T, typename F>
+void iter(T *arr, unsigned long lenght, F fun)
 {
     for (unsigned long i = 0; i < lenght; i++)
         fun(arr[i]);
@@ -13,6 +13,7 @@ template <typename A>
 void printNumber(A a) {
     cout << a << endl;
 }
+
 int main()
 {
     int arrInt[] = {1, 2, 3, 4, 5};
@@ -20,11 +21,11 @@ int main()
 
     cout << "Integers:  " << endl;
     unsigned long lenght = sizeof(arrInt) / sizeof(int); 
-    iter(arrInt, lenght, printNumber);
+    iter(arrInt, lenght, printNumber<int>);
 
     double arrDou[] = {1.1, 2.2, 3.3, 4.4, 5.5};
     cout << "Double:  "<< endl;
     unsigned long lenght2 = sizeof(arrDou) / sizeof(double); 
-    iter(arrDou, lenght2, printNumber);
+    iter(arrDou, lenght2, printNumber<double>);
 
 }
