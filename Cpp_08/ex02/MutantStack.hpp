8b56043fbbe14cc1e274854bc20a5ad2a02cf97f
Hatice Nur Eris <110325247+hnur-eris@users.noreply.github.com>
2024-02-19 21:11:54 +0300

    .

--- Cpp_08/ex02/MutantStack.hpp
+++ Cpp_08/ex02/MutantStack.hpp
@@ -7,32 +7,20 @@ template <typename T>
 class MutantStack : public std::stack<T> {
 
     public :
-        MutantStack() {}
-        MutantStack(const MutantStack &obj){
+        MutantStack() : std::stack<T>() {}
+        MutantStack(const MutantStack &obj) : std::stack<T>(obj){
             *this = obj;
         }
         MutantStack &operator = (const MutantStack &obj) {
+            std::stack<T>::operator=(obj);
             return *this;
         }
         ~MutantStack() {}
 
         typedef typename std::stack<T>::container_type::iterator iterator;
-        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
-        typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
         iterator begin(){
             return this->c.begin();
-        }
-        const_iterator begin() const {
-            return this->c.begin();
-        }
-
-        reverse_iterator rbegin() {
-            return this->c.rbegin();
-        }
-        const_reverse_iterator rbegin() const {
-            return this->c.rbegin();
         }
 
         iterator end(){
